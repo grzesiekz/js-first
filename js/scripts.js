eb90c5b6c617bfaea2c0501b7c39b020b686c57f
Grzegorz Żuchowicz <grzegorz@zuchowicz.net>
2016-10-30 09:36:22 +0100

    Triangle area

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var a = prompt('Podaj długość przyprostokątnej');
+var h = prompt('Podaj wysokość trójkąta');
+var triangleArea = (a*h)/2;
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+
+// Oczywiście, jeśli użyszkodnik poda coś innego niż liczby jako a i h to program rzuci wyjątkiem. Zakładam, że nikt głupot nie będzie wpisywał. 
