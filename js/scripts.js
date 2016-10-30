56981687d0628d9d2705c1a666322a9c107fbec7
Grzegorz Żuchowicz <grzegorz@zuchowicz.net>
2016-10-30 10:10:13 +0100

    Zadanie 8.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var a, b, value;
+// można było od razu zrobić var a = prompt('');..., ale w zadaniu było, żeby osobno zadeklarować, więc jest jak jest
+a = prompt('Podaj a');
+b = prompt('Podaj b');
+value = (a*a) + (2*a*b) - (b*b);
+console.log('Wynik działania: ', value);
+if (value > 0) {
+	console.log('Wynik dodatni')
+} else if (value < 0) {
+	console.log('Wynik ujemny')
+} else {
+	console.log('ZERO')
+}
+
+
